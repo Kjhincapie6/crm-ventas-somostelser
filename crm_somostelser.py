5e5e611c75582bbb6e9f7979308f3c9cb658f81c
Kely Jhojana Hincapie Zapata <kely.hincapie@uniminuto.edu.co>
2026-06-28 21:40:10 -0500

    Update page title in crm_somostelser.py

--- crm_somostelser.py
+++ crm_somostelser.py
@@ -12,7 +12,7 @@ import random
 
 # ─── CONFIGURACIÓN DE PÁGINA ───────────────────────────────
 st.set_page_config(
-    page_title="CRM Somos Telser",
+    page_title="SaaS De Ventas Somos Telser",
     page_icon="📡",
     layout="wide",
     initial_sidebar_state="expanded"
