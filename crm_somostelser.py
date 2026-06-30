c037d6052db4c1fc5f841e1ed652dd68caad0eec
Kely Jhojana Hincapie Zapata <kely.hincapie@uniminuto.edu.co>
2026-06-30 12:01:52 -0500

    Update crm_somostelser.py

--- crm_somostelser.py
+++ crm_somostelser.py
@@ -806,6 +806,7 @@ def tab_registrar_venta():
                 })
                 st.success(f"✅ Línea {num_linea} agregada.")
                 st.session_state["num_linea_pop"] = ""
+            if "serial_chip_pop" in st.session_state:
                 st.session_state["serial_chip_pop"]
                 st.rerun()
 
